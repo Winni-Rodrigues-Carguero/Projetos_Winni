e1c5abc77029c89f19e9a99ebac56391e3465ed0
Winni Rodrigues <winnirodrigues@MacBook-Pro-de-Winni.local>
2021-06-03 17:41:28 -0300

    Novo arquivo

---
+++ operadores_relacionais.rb
@@ -0,0 +1,27 @@
+interger = 20 #numero intereiro
+float = 5.5  #numero real/ numeros quebrados
+text = 'Sou demais'
+=begin
+if interger > 50 
+    puts 'A Variável é maior'
+else 
+    puts 'A Variável  é menor'
+end
+
+if text == 'Sou demais' 
+    puts 'Os textos são iguais'
+else
+    puts 'Os textos são diferentes'
+end
+
+if text != 'Sou demais' 
+    puts 'Os textos são iguais'
+else
+    puts 'Os textos são diferentes'
+end
+=end
+if interger > 30 && text == 'Sou demais'
+    puts 'As duas comparações são verdadeiras'
+else
+    puts 'Alguma comparação é falsa'
+end
